503d8bb9ad5b5c58a02cfb51c5fdda078f3f08b9
Peter <piotrmgrupa4@gmail.com>
2023-04-18 14:41:38 +0200

    Section 4 Vid 8 Agent Movement Speed FIX

--- AgentMover.cs
+++ AgentMover.cs
@@ -8,13 +8,15 @@ namespace FarmGame.Agent
 {
     public class AgentMover : MonoBehaviour
     {
-        public Vector2 MovementInput { get; set; }
+        public Vector2 MovementInput { get; private set; }
         public Transform _testTransform;
         public event Action<bool> OnMove;
+        [SerializeField]
+        private float speed = 2;
 
         private void Update()
         {
-            _testTransform.position += (Vector3)MovementInput * Time.deltaTime;
+            _testTransform.position += (Vector3)MovementInput*speed * Time.deltaTime;
         }
 
         internal void SetMovementInput(Vector2 input)
