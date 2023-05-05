cd2bf8479db6b0a7871ee76a8322c31ae3974815
Peter <piotrmgrupa4@gmail.com>
2023-05-05 14:12:43 +0200

    Section 8 Video 4 Interaction Detector

---
+++ Interactions/InteractionDetector.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FarmGame.Interactions
+{
+    public class InteractionDetector : MonoBehaviour
+    {
+        [SerializeField]
+        private LayerMask _interactionLayerMask;
+        [SerializeField, Range(0.1f,1)]
+        private float _interactionDistance = 0.5f;
+
+        private Vector2 _interactionDirection;
+
+        public void SetInteractionDirection(Vector2 input)
+        {
+            if(input.magnitude > 0.1f)
+                _interactionDirection = input.normalized;
+        }
+
+        public IEnumerable<PickUpInteraction> PerformDetection()
+        {
+            Collider2D collisionResult 
+                = Physics2D.OverlapCircle(
+                    (Vector2)transform.position + _interactionDirection*_interactionDistance,
+                    0.1f,
+                    _interactionLayerMask);
+            if(collisionResult != null)
+            {
+                return collisionResult.GetComponents<PickUpInteraction>();
+            }
+            return new List<PickUpInteraction>();
+        }
+    }
+}
--- Player.cs
+++ Player.cs
@@ -1,4 +1,5 @@
 using FarmGame.Input;
+using FarmGame.Interactions;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -15,10 +16,14 @@ namespace FarmGame.Agent
         [SerializeField]
         private AgentAnimation _agentAnimation;
 
+        [SerializeField]
+        private InteractionDetector _interactionDetector;
+
         private void OnEnable()
         {
             _agentInput.OnMoveInput.AddListener(_agentMover.SetMovementInput);
             _agentInput.OnMoveInput.AddListener(_agentAnimation.ChangeDirection);
+            _agentInput.OnMoveInput.AddListener(_interactionDetector.SetInteractionDirection);
             _agentInput.OnPerformAction += PerformAction;
 
             _agentMover.OnMove += _agentAnimation.PlayMovementAnimation;
@@ -27,12 +32,20 @@ namespace FarmGame.Agent
         private void PerformAction()
         {
             Debug.Log("Interacting");
+            foreach (PickUpInteraction item in _interactionDetector.PerformDetection())
+            {
+                if (item.CanInteract())
+                {
+                    item.Interact(this);
+                }
+            }
         }
 
         private void OnDisable()
         {
             _agentInput.OnMoveInput.RemoveListener(_agentMover.SetMovementInput);
             _agentInput.OnMoveInput.RemoveListener(_agentAnimation.ChangeDirection);
+            _agentInput.OnMoveInput.RemoveListener(_interactionDetector.SetInteractionDirection);
             _agentInput.OnPerformAction -= PerformAction;
 
             _agentMover.OnMove -= _agentAnimation.PlayMovementAnimation;
