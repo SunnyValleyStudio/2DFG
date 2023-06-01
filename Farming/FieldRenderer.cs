7cb36919835ff73027b78c37b06f34d7ddf4471e
Peter <piotrmgrupa4@gmail.com>
2023-06-01 12:19:58 +0200

    Section 11 Vid 2 Field Renderer

---
+++ Farming/FieldRenderer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace FarmGame
+{
+    public class FieldRenderer : MonoBehaviour
+    {
+        [SerializeField]
+        private Tilemap _preparedFieldTilemap;
+
+        [SerializeField]
+        private TileBase _preparedFieldTile;
+
+        public Vector3Int GetTilemapTilePosition(Vector3 worldPosition)
+            => _preparedFieldTilemap.WorldToCell(worldPosition);
+
+        public void PrepareFieldAt(Vector3Int fieldCellPosition)
+        {
+            _preparedFieldTilemap.SetTile(fieldCellPosition,_preparedFieldTile);
+        }
+    }
+}
