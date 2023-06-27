60a0c8089300cf4ab68326da7d47966852ba9399
Peter <piotrmgrupa4@gmail.com>
2023-06-27 11:13:43 +0200

    Section 15 Vid 6 PickUp Item Data

---
+++ Data Storage/ItemData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FarmGame.DataStorage
+{
+    public class ItemData : MonoBehaviour
+    {
+        public int itemDatabaseIndex;
+        public int itemCount = 1;
+        public int itemQuality = -1;
+    }
+}
--- Interactions/PickUpInteraction.cs
+++ Interactions/PickUpInteraction.cs
@@ -1,4 +1,5 @@
 using FarmGame.Agent;
+using FarmGame.DataStorage;
 using FarmGame.DataStorage.Inventory;
 using FarmGame.Tools;
 using System.Collections;
@@ -8,8 +9,15 @@ using UnityEngine.Events;
 
 namespace FarmGame.Interactions
 {
+    [RequireComponent(typeof(ItemData))]
     public class PickUpInteraction : MonoBehaviour, IInteractable
     {
+        private ItemData _data;
+        [SerializeField]
+        private bool _destroyAfterPickup = true;
+        [field: SerializeField]
+        public ItemDatabaseSO ItemDatabase { get; set; }
+
         [field: SerializeField]
         public List<ToolType> UsableTools { get; set; }
             = new List<ToolType>();
@@ -21,10 +29,28 @@ namespace FarmGame.Interactions
 
         public void Interact(IAgent agent)
         {
-            agent.Inventory.AddItem(new InventoryItemData(0, 1, -1), 1);
-            Debug.Log(agent.Inventory);
-            OnPickUp?.Invoke();
-            Destroy(gameObject);
+            //agent.Inventory.AddItem(new InventoryItemData(0, 1, -1), 1);
+            InventoryItemData item = new InventoryItemData(_data.itemDatabaseIndex,
+                _data.itemCount,
+                _data.itemQuality);
+            ItemDescription description = ItemDatabase.GetItemData(_data.itemDatabaseIndex);
+            int stackSize = description.CanBeStacked ? description.StackQuantity : -1;
+            if(agent.Inventory != null && _data.itemCount > 0
+                && agent.Inventory.IsThereSpace(item, stackSize))
+            {
+                agent.Inventory.AddItem(item, stackSize);
+                Debug.Log(agent.Inventory);
+                OnPickUp?.Invoke();
+                if(_destroyAfterPickup)
+                {
+                    Debug.Log($"Destroying {description.Name} {gameObject.name}");
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    _data.itemCount = 0;
+                }
+            }
         }
     }
 }
