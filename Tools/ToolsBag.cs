dc2c5066dac3241d258f44f46badc2e070bd2d7c
Peter <piotrmgrupa4@gmail.com>
2023-08-02 12:16:18 +0200

    Section 18 Vid 2 Reusing Inventory class in Tools Bag

--- Tools/ToolsBag.cs
+++ Tools/ToolsBag.cs
@@ -1,5 +1,6 @@
 using FarmGame.Agent;
 using FarmGame.DataStorage;
+using FarmGame.DataStorage.Inventory;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -16,11 +17,55 @@ namespace FarmGame.Tools
 
         [SerializeField]
         private List<int> _initialTools;
+        [SerializeField]
+        private Inventory _toolsBagInventory;
 
-        private Tool _currentTool;
+        private List<Tool> _newBag;
 
-        public Tool CurrentTool => _currentTool;
+        [SerializeField]
+        private int _handToolID = 4;
+
+        public Tool CurrentTool => _newBag[_selectedIndex];
         public event Action<int, List<Sprite>, int?> OnToolsBagUpdated;
+
+        private void Start()
+        {
+            for (int i = 0; i < _initialTools.Count; i++)
+            {
+                ItemDescription description = _itemDatabase.GetItemData(_initialTools[i]);
+                _toolsBagInventory.AddItem(new InventoryItemData(description.ID, 1, -1, null),
+                    description.StackQuantity);
+            }
+            UpdateToolsBag(_toolsBagInventory.InventoryContent);
+        }
+
+        private void UpdateToolsBag(IEnumerable<InventoryItemData> inventoryContent)
+        {
+            _newBag = new();
+            AddDefaultHandTool();
+            foreach (InventoryItemData tool in inventoryContent) 
+            { 
+                if(tool != null)
+                {
+                    ItemDescription toolDescription = _itemDatabase.GetItemData(tool.id);
+                    if(toolDescription == null || toolDescription.ToolType == ToolType.None)
+                    {
+                        Debug.LogError($"LOaded tool with index {tool.id} is not present in database or None");
+                    }
+                    Tool newTool = ToolFactory.CreateTool(toolDescription, tool.data);
+                    _newBag.Add(newTool);
+                }
+            }
+            if(_selectedIndex >= _newBag.Count)
+                _selectedIndex = 0;
+        }
+
+        private void AddDefaultHandTool()
+        {
+            ItemDescription handToolDescription = _itemDatabase.GetItemData(_handToolID);
+            Tool handTool = ToolFactory.CreateTool(handToolDescription, null);
+            _newBag.Add(handTool); // handTOol -> 0
+        }
 
         public void Initialize(IAgent agent)
         {
@@ -34,18 +79,19 @@ namespace FarmGame.Tools
 
         private void SwapTool(int newIndex, IAgent agent)
         {
-            if(_currentTool != null)
+            if(_newBag[_selectedIndex] != null)
             {
                 PutAway(agent);
             }
             _selectedIndex = newIndex;
-            if(_selectedIndex >= _initialTools.Count)
+            if(_selectedIndex >= _newBag.Count)
             {
                 _selectedIndex = 0;
             }
-            ItemDescription description = _itemDatabase.GetItemData(_initialTools[_selectedIndex]);
+            ItemDescription description 
+                = _itemDatabase.GetItemData(_newBag[_selectedIndex].ItemIndex);
             Debug.Log($" Equipped tool: {description.Name}");
-            _currentTool = ToolFactory.CreateTool(description);
+            //_newBag[_selectedIndex] = ToolFactory.CreateTool(description);
             EquipTool(agent);
             SendUpdateMessage();
         }
@@ -53,9 +99,9 @@ namespace FarmGame.Tools
         private void SendUpdateMessage()
         {
             List<Sprite> sprites = new List<Sprite>();
-            foreach (int ID in _initialTools)
+            foreach (Tool tool in _newBag)
             {
-                ItemDescription toolDescription = _itemDatabase.GetItemData(ID);
+                ItemDescription toolDescription = _itemDatabase.GetItemData(tool.ItemIndex);
                 if(toolDescription != null)
                 {
                     sprites.Add(toolDescription.Image);
@@ -66,12 +112,12 @@ namespace FarmGame.Tools
 
         private void EquipTool(IAgent agent)
         {
-            _currentTool.Equip(agent);
+            _newBag[_selectedIndex].Equip(agent);
         }
 
         private void PutAway(IAgent agent)
         {
-            _currentTool.PutAway(agent);
+            _newBag[_selectedIndex].PutAway(agent);
         }
     }
 }
