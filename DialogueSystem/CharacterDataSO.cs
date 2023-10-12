c6eebc8fe27b65cbdc7161a69e2d7e303e196a1b
Peter <piotrmgrupa4@gmail.com>
2023-10-12 11:12:43 +0200

    Section 30 Vid 3 Dialogue Data

---
+++ DialogueSystem/CharacterDataSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FarmGame.DialogueSystem
+{
+    [CreateAssetMenu]
+    public class CharacterDataSO : ScriptableObject
+    {
+        public Sprite Portrait;
+        public string CharacterName;
+    }
+}
---
+++ DialogueSystem/DialogueData.cs
@@ -0,0 +1,23 @@
+using FarmGame.Agent;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FarmGame.DialogueSystem
+{
+    public class DialogueData : MonoBehaviour
+    {
+        [field: SerializeField]
+        public CharacterDataSO CharacterData { get; private set; }
+        [field: SerializeField]
+        public string Text { get; set; }
+
+        public UnityEvent<IAgent> OnActon;
+
+        [field: SerializeField]
+        public DialogueData NextDialogue { get; private set; }
+
+
+    }
+}
